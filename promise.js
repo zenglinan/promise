93a2e7eb4a2e07197a2a1991b673e7d70dbac71f
zbh <37680843+zenglinan@users.noreply.github.com>
2019-10-29 00:44:25 +0800

    实现 promise 基本结构和三种状态切换，实现基本的 then 方法

---
+++ promise.js
@@ -0,0 +1,42 @@
+// const promise = new MyPromise((resolve, reject) => {
+//   reject('123')
+//   resolve(123)
+// })
+
+// promise.then((value) => {
+//   console.log('success', value)  
+// }, (reson) => {
+//   console.log('fail', reson)
+// })
+
+// 实现功能：
+// 1. 实现 promise 基本结构和三种状态切换
+// 2. 实现基本的 then 方法，根据状态判断执行的回调
+
+class MyPromise{
+  constructor(executor){
+    this.value = ''  // resolve 的值
+    this.reson = ''  // reject 的值
+    this.state = 'pedding'  // 当前 Promise 的状态
+    const resolve = (value) => {  // 写成箭头函数，保证 this
+      if(this.state === 'pedding'){  // 只有 pedding 态可修改，并且不可逆
+        this.state = 'fulfilled'
+        this.value = value
+      }
+    }
+    const reject = (reson) => {
+      if(this.state === 'pedding'){
+        this.state = 'rejected'
+        this.reson = reson
+      }
+    }
+    executor(resolve, reject)
+  }
+  then(onfulfilled, onrejected){  // 根据 promise 状态执行对应回调
+    if(this.state === 'fulfilled'){
+      onfulfilled(this.value)
+    }else if(this.state === 'rejected'){
+      onrejected(this.reson)
+    }
+  }
+}
